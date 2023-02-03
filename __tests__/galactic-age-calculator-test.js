66708cc612074984f953b9f8612fb73882cee709
John-Dig <rohnfiggins@gmail.com>
2023-02-03 11:55:54 -0800

    4.1 re written

--- __tests__/galactic-age-calculator-test.js
+++ __tests__/galactic-age-calculator-test.js
@@ -12,29 +12,33 @@ describe('Calculator, and beforeEach', () => {
     theCalculator.user1.userBirthday = "01/01/1923"
     //add to this later
   })  
-  //test 1.2  
-  test('object Calculator exists', () => {
-    const theCalculator = new Calculator();
-    expect(theCalculator).toBeTruthy();
-  });
+  // //test 1.2  
+  // test('object Calculator exists', () => {
+  //   const theCalculator = new Calculator();
+  //   expect(theCalculator).toBeTruthy();
+  // });
 
-  //test 2.2
-  describe('User', () => {
-  });
-  test('should create new user Object', () => {
-    expect(theCalculator.user1.userName).toEqual("Rod Steiger");
-    expect(theCalculator.user1.userAge).toEqual(100);
-    expect(theCalculator.user1.userBirthday).toEqual("01/01/1923");
-  });
+  // //test 2.2
+  // describe('User', () => {
+  // });
+  // test('should create new user Object', () => {
+  //   expect(theCalculator.user1.userName).toEqual("Rod Steiger");
+  //   expect(theCalculator.user1.userAge).toEqual(100);
+  //   expect(theCalculator.user1.userBirthday).toEqual("01/01/1923");
+  // });
 
-  //test 3.1
-  test('should return age in mercury years from input in earth years', () => {
-    expect(theCalculator.mercury(100)).toEqual(24)
-    console.log(theCalculator);
-  })
+  // //test 3.1
+  // test('should return age in mercury years from input in earth years', () => {
+  //   expect(theCalculator.mercury(100)).toEqual(24)
+  //   console.log(theCalculator);
+  // })
 
-  //test 4
+  //test 4.1
   test('should return age in all 4 planet years from input in earth years', () => {
+    theCalculator.user1.mercuryUserAge = theCalculator.mercury(user1.userAge);
+    theCalculator.user1.venusUserAge = theCalculator.venus(user1.userAge);
+    theCalculator.user1.marsUserAge = theCalculator.mars(user1.userAge);
+    theCalculator.user1.jupiterUserAge = theCalculator.jupiter(user1.userAge);
     expect(theCalculator.user1.mercuryUserAge).toEqual(theCalculator.user1.userAge * .24);
     expect(theCalculator.user1.venusUserAge).toEqual(theCalculator.user1.userAge *.62);
     expect(theCalculator.user1.marsUserAge).toEqual(theCalculator.user.userAge * 1.88);
