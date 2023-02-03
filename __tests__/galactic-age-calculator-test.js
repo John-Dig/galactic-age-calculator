c0a2d11c1320473fe99ca6c06cee02120de9889a
John-Dig <rohnfiggins@gmail.com>
2023-02-03 10:06:53 -0800

    2 written

--- __tests__/galactic-age-calculator-test.js
+++ __tests__/galactic-age-calculator-test.js
@@ -1,24 +1,21 @@
 import {Calculator} from './../src/js/calculator';
+import {User} from './../src/js/user';
 
 //test 1
 describe('Calculator', () => {
 });
 test('object Calculator exists', () => {
-  console.log (Calculator);
   const theCalculator = new Calculator("hi");
-  console.log(theCalculator);
   
   //expect(theCalculator).toBeTruthy();
 });
 
-// //test 1.3
-// describe('Avatar', () => {
-// });
-// test('should create new Avatar object', () => {
-//   const hero = new Avatar();
-//   expect(hero.karma).toEqual(undefined);
-//   expect(hero.physicalA).toEqual(undefined);
-//   expect(hero.health).toEqual(undefined);
-//   expect(hero.experience).toEqual(undefined);
-//   expect(hero.inventory).toEqual(undefined);
-// });
+//test 2
+describe('User', () => {
+});
+test('should create new user Object', () => {
+  const user1 = new User();
+  expect(user1.userName).toEqual(undefined);
+  expect(user1.userAge).toEqual(undefined);
+  expect(user1.userBirthday).toEqual(undefined);
+});
