001a034bedf2e9cd3550d19f9fe6a3bfa5cfdbd4
John-Dig <rohnfiggins@gmail.com>
2023-02-03 09:20:32 -0800

    1 written

--- __tests__/galactic-age-calculator-test.js
+++ __tests__/galactic-age-calculator-test.js
@@ -1,4 +1,14 @@
-// import Object from './../src/TEMPLATE.js';
+import Calculator from './../src/calculator.js';
+
+describe('Calculator', () => {
+});
+//test 1
+test('object Calculator exists', () => {
+  const theCalculator = new Calculator;
+  expect(theCalculator).toBeTruthy();
+});
+
+// import Object from './../src/calculator.js';
 // describe('OBJECT', () => {
 // });
 // test('DESCRIBE TEST', () => {
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './css/styles.css';
+//import Calculator from "./js/Calculator";
+//import User from "./js/User.js";
